a545f38b75b9c33f2fb81e9b69d0455fcda88bd1
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-02-10 21:52:07 +0530

    Landing done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Landing from './Files/Landing';
+import Login from './Files/User/Login';
+import Register from './Files/User/Register';
+const App=()=>{
   return (
-    <div className="App">
-      HWLLOW
-      I AM DONE
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path='*' element={<Navigate to="/" />} />
+      </Routes>
+      </Router>
   );
 }
 
