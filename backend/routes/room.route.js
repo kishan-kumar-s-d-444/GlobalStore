2f4f703c58dd62c7b9520510a23ba669b6c110e0
KISHAN KUMAR S D <kishankumarsd444@gmail.com>
2025-04-13 14:09:03 +0530

    CORS ERROR SOLVING 10

--- backend/routes/room.route.js
+++ backend/routes/room.route.js
@@ -5,8 +5,8 @@ import { verifyToken } from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 router.route("/createroom").post(verifyToken, upload.roomImage, createRoom);
-router.route("/myrooms").get(verifyToken, getAllRooms);
-router.route("/single/:roomId").get(verifyToken, getSingleRoom);
+router.route("/myrooms").get(getAllRooms);
+router.route("/single/:roomId").get(getSingleRoom);
 router.route("/public").get(getPublicRooms);
 router.post("/join/:roomId", verifyToken, joinRoom);
 router.patch("/:roomId/make-public", verifyToken, makeRoomPublic);
